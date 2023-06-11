538cd0050fc95e8fb24ea5be9170068b1b0b3a56
Vaishali <v.sirimalla@somaiya.edu>
2023-06-11 19:37:25 +0530

    Second commit

--- app/src/main/java/com/example/hourlytemperature/WeatherService.kt
+++ app/src/main/java/com/example/hourlytemperature/WeatherService.kt
@@ -5,17 +5,11 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherService {
-    @GET("onecall")
+    @GET("weather")
     fun getHourlyWeather(
         @Query("lat") latitude: Double,
         @Query("lon") longitude: Double,
         @Query("exclude") exclude: String = "current,minutely,daily,alerts",
         @Query("appid") apiKey: String
-    ): Call<HourlyWeatherResponse>
-
-    fun getHourlyWeather(
-        latitude: Double,
-        longitude: Double,
-        exclude: String
     ): Call<HourlyWeatherResponse>
 }
